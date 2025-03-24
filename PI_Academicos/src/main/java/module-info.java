dfeea1926d473d3c7d67dea27cfdfbc8b471deff
annejunkes <anneburkhardtjunkes@gmail.com>
2025-03-24 11:59:50 -0300

    tela login

--- PI_Academicos/src/main/java/module-info.java
+++ PI_Academicos/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mycompany.telasacademicosanne {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens controller to javafx.fxml;
     exports principal;
