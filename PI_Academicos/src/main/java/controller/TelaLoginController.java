7e467d7571baaf75171924667dc58f4bc1bece7d
annejunkes <anneburkhardtjunkes@gmail.com>
2025-03-25 11:38:09 -0300

    classe app

--- PI_Academicos/src/main/java/controller/TelaLoginController.java
+++ PI_Academicos/src/main/java/controller/TelaLoginController.java
@@ -1,5 +1,53 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
 public class TelaLoginController {
     
+    private Stage stageLogin;
+                     
+     @FXML
+    private Button btnEntrar;
+
+    @FXML
+    private Label lblCadastro;
+
+    @FXML
+    private Text lblRecuperarSenha;
+
+    @FXML
+    private TextField txtNome;
+
+    @FXML
+    private PasswordField txtSenha;
+
+    @FXML
+    void onClickCadastro(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onClickEntrar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickRecuperarSenha(MouseEvent event) {
+
+    }
+    
+    public void abrirJanela(){
+        System.out.println("controller.TelaLoginController.abrirJanela()");
+    }
+    public void setStage(Stage stage){
+        this.stageLogin = stage;
+    }
 }
--- PI_Academicos/src/main/java/principal/App.java
+++ PI_Academicos/src/main/java/principal/App.java
@@ -1,34 +1,43 @@
 package principal;
 
+import controller.TelaLoginController;
+import java.io.File;
 import javafx.application.Application;
+import java.io.IOException;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 
-/**
- * JavaFX App
- */
 public class App extends Application {
-
-    private static Scene scene;
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
+    
+        stage.setTitle("Tela inicial de login");
+        
+        URL url = new File("src/main/java/view/TelaLogin.fxml").toURI().toURL();
+        
+        FXMLLoader loader = new FXMLLoader(url);
+        
+        Parent root = loader.load();
+        
+        Stage telaLogin = new Stage();
+        
+        TelaLoginController tlc = loader.getController();
+        tlc.setStage(telaLogin);
+        
+        telaLogin.setOnShown(event -> {
+            tlc.abrirJanela();
+        });
+        
+        
+        Scene cena = new Scene(root);
+        telaLogin.setScene(cena);
+        telaLogin.show();
     }
 
     public static void main(String[] args) {
