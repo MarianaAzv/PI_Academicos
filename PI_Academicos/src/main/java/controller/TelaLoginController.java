e19670f2aecc58df5dc2a11cfb49c873d97248bc
annejunkes <anneburkhardtjunkes@gmail.com>
2025-03-25 15:28:26 -0300

    transição da tela login para cadastro

--- PI_Academicos/src/main/java/controller/TelaLoginController.java
+++ PI_Academicos/src/main/java/controller/TelaLoginController.java
@@ -1,7 +1,13 @@
 package controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -30,12 +36,26 @@ public class TelaLoginController {
     private PasswordField txtSenha;
 
     @FXML
-    void onClickCadastro(MouseEvent event) {
+    void onClickCadastro(MouseEvent event) throws IOException {
+        
+        URL url = new File("src/main/java/view/CadastroCoordenador.fxml").toURI().toURL();       
+        FXMLLoader loader = new FXMLLoader(url);
+        
+        Parent root = loader.load();
+        
+        Stage stage = new Stage();
+        
+        CadastroCoordenadorController cc = loader.getController();
+        
+        Scene cena = new Scene(root);
+        stage.setTitle("Cadastro Coordenador");
+        stage.setScene(cena);
+        stage.show();
 
     }
 
     @FXML
-    void onClickEntrar(ActionEvent event) {
+    void onClickEntrar(ActionEvent event) throws IOException {
 
     }
 
