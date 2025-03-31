948570fd9c90cb0b2d1d7f0363c45211c2ab1f14
Fernanda Cardoso Serafim <fernanda.cs20@aluno.ifsc.edu.br>
2025-03-31 12:16:18 -0300

    Administrador, Bolsista e coordenador

---
+++ PI_Academicos/src/main/java/model/Bolsista.java
@@ -0,0 +1,17 @@
+
+package model;
+
+import java.time.LocalDate;
+
+
+public class Bolsista extends Usuario {
+    
+    private long matricula;
+    private String curso;
+    private LocalDate dataInicio;
+    private LocalDate dataFim;
+    private boolean acessoPostagens;
+    private boolean acessoArtigos;
+    
+    
+}
