bbd84d00891a7a70f5c5ebc58db840545580a156
mariana <mariana.a2006@aluno.ifsc.edu.br>
2025-03-31 12:12:44 -0300

    Cadastro cord controller  estrutura

--- PI_Academicos/src/main/java/controller/CadastroCoordenadorController.java
+++ PI_Academicos/src/main/java/controller/CadastroCoordenadorController.java
@@ -4,10 +4,72 @@
  */
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
 /**
  *
  * @author Aluno
  */
 public class CadastroCoordenadorController {
-    
+       @FXML
+    private Button btnSubmeter;
+
+    @FXML
+    private ImageView imgLogo;
+
+    @FXML
+    private Label lblCPF;
+
+    @FXML
+    private Label lblCadastro;
+
+    @FXML
+    private Label lblEmail;
+
+    @FXML
+    private Label lblFormatacao;
+
+    @FXML
+    private Label lblMensagemsdeSubmissaoparte1;
+
+    @FXML
+    private Label lblMensagemsdeSubmissaoparte2;
+
+    @FXML
+    private Label lblNomeCompleto;
+
+    @FXML
+    private Label lblSIAPE;
+
+    @FXML
+    private Label lblSenha;
+
+    @FXML
+    private Label lblUsuario;
+
+    @FXML
+    private TextField txtCPF;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtFormatacao;
+
+    @FXML
+    private TextField txtNomeCompleto;
+
+    @FXML
+    private TextField txtSIAPE;
+
+    @FXML
+    private PasswordField txtSenha;
+
+    @FXML
+    private TextField txtUsuario;   
 }
