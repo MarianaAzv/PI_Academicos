7a0f21d3adde24994cf1608195896d8346e696e2
MarianaAzv <mariana.a2006@aluno.ifsc.edu.br>
2025-04-07 11:28:38 -0300

    CriarProjeto concluido a tela

--- PI_Academicos/src/main/java/controller/CriarProjetoController.java
+++ PI_Academicos/src/main/java/controller/CriarProjetoController.java
@@ -1,14 +1,92 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 
-/**
- *
- * @author Aluno
- */
 public class CriarProjetoController {
-     
+
+    @FXML
+    private ComboBox<?> CBcategoria;
+
+    @FXML
+    private Label LblDatadeinicio;
+
+    @FXML
+    private Button btnEnviar;
+
+    @FXML
+    private Button btnNaococoordenador;
+
+    @FXML
+    private Button btnPDF;
+
+    @FXML
+    private Button btnSimBolsista;
+
+    @FXML
+    private Button btnSimCocoordenador;
+
+    @FXML
+    private Button btnnaoBolsista;
+
+    @FXML
+    private ImageView imgLogo;
+
+    @FXML
+    private Label lblBolsista;
+
+    @FXML
+    private Label lblCampus;
+
+    @FXML
+    private Label lblCategoria;
+
+    @FXML
+    private Label lblCocoordenador;
+
+    @FXML
+    private Label lblCoordenador;
+
+    @FXML
+    private Label lblDatadeFim;
+
+    @FXML
+    private Label lblEdital;
+
+    @FXML
+    private Label lblNomedoprojeto;
+
+    @FXML
+    private Label lblResumo;
+
+    @FXML
+    private Label lbltitulocriarprojeto;
+
+    @FXML
+    private TextField txtCampus;
+
+    @FXML
+    private TextField txtCoordenador;
+
+    @FXML
+    private TextField txtDatadeFim;
+
+    @FXML
+    private TextField txtDatadeInicio;
+
+    @FXML
+    private TextField txtEdital;
+
+    @FXML
+    private TextField txtNomedoProjeto;
+
+    @FXML
+    private TextArea txtResumo;
+
 }
+
