b58170e0bb91e50434739dcc45d791c53075bbc9
jaquelinkkkkk <jaqueline.c2004@aluno.ifsc.edu.br>
2025-04-07 11:55:53 -0300

    Carregar tela de ver perfil
    Não está concluída vai da6 errado mesmoooooo

--- PI_Academicos/src/main/java/controller/TelaPrincipalCoordenadorController.java
+++ PI_Academicos/src/main/java/controller/TelaPrincipalCoordenadorController.java
@@ -1,17 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+//import model.Coordenador;
+import model.Usuario;
 
 public class TelaPrincipalCoordenadorController {
+    
+      private Stage stageTelaPrincipalCoordenador;
+    private Connection conexao;
+    private final Usuario dao = new Usuario();
+    private ArrayList<String> listaDados;
+    private Usuario user;
 
     @FXML
     private Text TxtNomeUsuario;
@@ -40,8 +55,8 @@ public class TelaPrincipalCoordenadorController {
     @FXML
     private Button btnVerProjeto;
 
-    @FXML
-    private Button btnVerperfil;
+   @FXML
+    private Button btnVerPerfil;
 
     @FXML
     private ImageView imgLogo;
@@ -99,5 +114,68 @@ public void setStage(Stage stage){
         this.stageLogin = stage;
     }
 
-}
+       @FXML
+    void onClickVerPerfil(ActionEvent event) throws IOException {
+
+        //carregando a tela principal do coordenador
+        Usuario usuario = null; //talvez errado
+         if( usuario != null){
+            
+            URL url = new File("src/main/java/view/VerPerfilCoordenador.fxml").toURI().toURL();
+            FXMLLoader loader = new FXMLLoader(url);
+            Parent root = loader.load();
+        
+            Stage stage = new Stage();
+            
+            VerPerfilCoordenadorController vpf = loader.getController();
+            
+        
+            Scene cena = new Scene(root);
+            stage.setTitle("Perfil Coordenador");
+            stage.setScene(cena);
+            //deixa a tela maximizada
+            stage.setMaximized(true);
+            
+            stage.show();
+            stageTelaPrincipalCoordenador.close();
+            
+            }else {
+            
+        Alert alerta = new Alert(Alert.AlertType.INFORMATION);
+        alerta.setTitle("Nome ou senha incorretos");
+        alerta.setHeaderText("Verifique as informações inseridas");
+        alerta.setContentText("Digite novamente");
+        alerta.showAndWait();
+        
+    }
+            
+     
+      public void setStage(Stage stage){
+        this.stageTelaPrincipalCoordenador = stage;
+    }
+
+        
+    }
+   // void onClickAtualizarPerfil(){
+        
+   // }
+  //  void onClickCriarProjeto(){
+        
+   // }
+  //  void onClickSair(){
+        
+  //  }
+   // void onClickAdicionarPublicação(){
+        
+  //  }
+   // void onClickAdicionarArtigo(){
+        
+  //  }
+  //  void onClickAtualizarProjeto(){
+        
+ //   }
+   // void onClickOutrosProjetos(){
+        
+  //  }
+    
 
---
+++ PI_Academicos/src/main/java/controller/VerPerfilCoordenadorController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import javafx.stage.Stage;
+
+public class VerPerfilCoordenadorController {
+        private Stage stageTelaPrincipalCoordenador;
+    
+public void setStage(Stage stage){
+        this.stageTelaPrincipalCoordenador = stage;
+    }
+    
+}
