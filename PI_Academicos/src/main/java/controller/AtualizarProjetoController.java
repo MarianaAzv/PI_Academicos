019e095edcc8b1edd4597ab776d74eb669cd4453
Mariana <mariana.a2006@aluno.ifsc.edu.br>
2025-04-22 00:26:41 -0300

    Tela e  estrutura controller Atualizar Projeto

---
+++ PI_Academicos/src/main/java/controller/AtualizarProjetoController.java
@@ -0,0 +1,100 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class AtualizarProjetoController {
+
+    @FXML
+    private ComboBox<?> CBcategoria;
+
+    @FXML
+    private Label LblDatadeinicio;
+
+    @FXML
+    private Button btnAdicionarBolsista;
+
+    @FXML
+    private Button btnAdicionarCocoordenador;
+
+    @FXML
+    private Button btnAtualizar;
+
+    @FXML
+    private Button btnDesativarBolsista;
+
+    @FXML
+    private Button btnDesativarCocoordendor;
+
+    @FXML
+    private ImageView imgLogo;
+
+    @FXML
+    private Label lblBolsista;
+
+    @FXML
+    private Label lblCampus;
+
+    @FXML
+    private Label lblCategoria;
+
+    @FXML
+    private Label lblCocoordenador;
+
+    @FXML
+    private Label lblCoordenador;
+
+    @FXML
+    private Label lblDatadeFim;
+
+    @FXML
+    private Label lblEdital;
+
+    @FXML
+    private Label lblNomedoprojeto;
+
+    @FXML
+    private Label lblProrrogacao;
+
+    @FXML
+    private Label lblResumo;
+
+    @FXML
+    private Label lbltitulocriarprojeto;
+
+    @FXML
+    private TextField txtCampus;
+
+    @FXML
+    private TextField txtCoordenador;
+
+    @FXML
+    private TextField txtDatadeFim;
+
+    @FXML
+    private TextField txtDatadeInicio;
+
+    @FXML
+    private TextField txtEdital;
+
+    @FXML
+    private TextField txtNomedoProjeto;
+
+    @FXML
+    private TextField txtProrrogacao;
+
+    @FXML
+    private TextArea txtResumo;
+
+    @FXML
+    void OnClickEnviar(ActionEvent event) {
+
+    }
+
+}
