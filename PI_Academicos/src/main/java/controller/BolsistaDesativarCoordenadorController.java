760a837d62f7f4dc8ee2a40c8abd311c9468b29c
Mariana <mariana.a2006@aluno.ifsc.edu.br>
2025-06-02 20:54:10 -0300

    Estrutura criada

--- PI_Academicos/src/main/java/controller/BolsistaDesativarCoordenadorController.java
+++ PI_Academicos/src/main/java/controller/BolsistaDesativarCoordenadorController.java
@@ -1,26 +1,18 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
+
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import model.Projeto;
 
 public class BolsistaDesativarCoordenadorController {
 
-    @FXML
-    private CheckBox CBNomedoBolsistaMatricula;
-
-    @FXML
-    private CheckBox CBNomedoBolsistaMatricula2;
-
-    @FXML
-    private CheckBox CBNomedoBolsistaMatricula3;
-
-    @FXML
-    private CheckBox CBNomedoBolsistaMatricula4;
-
-    @FXML
-    private CheckBox CBNomedoBolsistaMatricula5;
+    private Stage stageBolsistaDesativar;
+    Projeto projeto;
 
     @FXML
     private Button btnConcluido;
@@ -28,5 +20,49 @@ public class BolsistaDesativarCoordenadorController {
     @FXML
     private Label lblBolsistas;
 
-}
+    @FXML
+    private VBox vbox;
 
+    //------------*SETs*---------//
+    public void setStage(Stage telaBolsistaDesativar) {
+        this.stageBolsistaDesativar = telaBolsistaDesativar;
+    }
+    
+    
+    public void setProjeto(Projeto projeto){
+        this.projeto = projeto;
+        SetarBolsistas();
+    }
+
+    //---------------*OnClick*---------------//
+    @FXML
+    void OnClickConcluido(ActionEvent event) {
+      DesativarBolsista();
+        
+        Concluido();
+    }
+
+    //--------------*Metodos*-------------//
+    
+    public void SetarBolsistas(){
+        
+        
+        
+          
+    }
+    
+    public void DesativarBolsista() {
+
+ 
+ 
+ 
+        
+        
+        
+        
+    }
+    
+    public void Concluido(){
+        
+    }
+}
