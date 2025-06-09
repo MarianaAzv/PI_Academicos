0b99e154ebdf0a2195fce9687ec791f14ebf9687
Anne Burkhardt Junkes <anneburkhardtjunkes@gmail.com>
2025-06-09 12:07:05 -0300

    ArtigoDAO - listarArtigos

--- PI_Academicos/src/main/java/model/ArtigoDAO.java
+++ PI_Academicos/src/main/java/model/ArtigoDAO.java
@@ -7,6 +7,8 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ArtigoDAO extends GenericDAO{
     
@@ -26,7 +28,7 @@ public class ArtigoDAO extends GenericDAO{
             stmt.setString(3, artigo.getAutores());
             stmt.setBytes(4, conteudoPDF);
             stmt.setDate(5, Date.valueOf(artigo.getDataPublicacao()));
-             stmt.setInt(6, artigo.getIdProjeto());
+            stmt.setInt(6, artigo.getIdProjeto());
             stmt.executeUpdate();
             
             ResultSet keys = stmt.getGeneratedKeys();
@@ -42,4 +44,47 @@ public class ArtigoDAO extends GenericDAO{
         }
     }
     
+    public List<Artigo> listarArtigos(Projeto projeto){
+        List<Artigo> artigos = new ArrayList<>();
+        String sql = "SELECT idArtigo, titulo, resumo, autores, arquivo, dataPublicacao, idProjeto FROM artigos WHERE idProjeto = ?;";
+        
+        Connection con = conectarDAO();
+        ResultSet rs = null;
+        
+        if(con!=null){
+        try {
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setInt(1, projeto.getIdProjeto());
+            rs = stmt.executeQuery();
+            
+            
+            int cont=0;
+            while (rs.next()) {
+                int idArtigo = rs.getInt("idArtigo");
+                String titulo = rs.getString("titulo");
+                String resumo = rs.getString("resumo");
+                String autores = rs.getString("autores");
+               // Timestamp timestamp = rs.getTimestamp("dataPublicacao"); 
+               // LocalDateTime data = timestamp.toLocalDateTime(); // convert timestamp pra localdatetime
+               // int idFoto = rs.getInt("idFoto");
+                byte[] dadosArtigo = rs.getBytes("arquivo");
+                //FileUtils.writeByteArrayToFile(new File("pathname"), myByteArray);
+              
+               // Foto foto = new Foto(idFoto, dadosImagem);
+               // Noticia noticia = new Noticia(idNoticia, idAdministrador, foto, titulo, texto, data);
+               // noticias.add(noticia);
+                //System.out.println("DAO: noticia carregada: " + noticia); // Imprime a foto carregada
+                //cont++;
+  
+        }
+            
+        
+        
+    }catch(SQLException e){
+        
+    }
+    
+}
+        return artigos;
+    }
 }
