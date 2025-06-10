4e2b133be0da16aa8c3a47d68fb67b6b0410c98a
Jaqueline <jaqueline.c2004@aluno.ifsc.edu.br>
2025-06-10 12:09:32 -0300

    ERRO controller senhas email

--- PI_Academicos/src/main/java/controller/RecuperarSenhaController.java
+++ PI_Academicos/src/main/java/controller/RecuperarSenhaController.java
@@ -4,6 +4,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.*;
+
 public class RecuperarSenhaController {
 
     @FXML
@@ -11,5 +15,39 @@ public class RecuperarSenhaController {
 
     @FXML
     private TextField txtEmailUsuario;
+
+    
+    public static void enviarEmail(String destinatario, String assunto, String mensagem) {
+        final String remetente = "seuemail@gmail.com"; // Seu e-mail
+        final String senha = "sua_senha"; // Sua senha ou App Password
+
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        Session session = Session.getInstance(props, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(remetente, senha);
+            }
+        });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(remetente));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(destinatario));
+            message.setSubject(assunto);
+            message.setText(mensagem);
+            Transport.send(message);
+            System.out.println("E-mail enviado com sucesso!");
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        enviarEmail("destinatario@gmail.com", "Recuperação de Senha", "Aqui está o seu código de recuperação: 123456");
+    }//ERRO
 
 }
